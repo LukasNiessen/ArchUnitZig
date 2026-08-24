e3ff98a0c34250771220d01ff99e1ddbf14101a8
Lukas Niessen <trolleidfischer2@gmail.com>
2026-08-24 02:56:06 +0200

    test(metrics): fix dependency fixture expectation

--- test/fixtures/metrics-dependency/src/a.zig
+++ test/fixtures/metrics-dependency/src/a.zig
@@ -3,5 +3,5 @@ const b = @import("b.zig");
 const c = @import("c.zig");
 
 test "a" {
-    try std.testing.expectEqual(@as(u8, 3), b.value + c.value);
+    try std.testing.expectEqual(@as(u8, 4), b.value + c.value);
 }
